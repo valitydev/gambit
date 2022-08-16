ec8644bd41c3eff99849d0e17b7c5f16408c2155
Vasil Khramov <vasil.khramov@gmail.com>
2022-08-16 14:42:03 +0300

    OPS-156: templator init

---
+++ src/test/java/com/empayre/gambit/GambitApplicationTest.java
@@ -0,0 +1,16 @@
+package dev.vality.gambit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = dev.vality.gambit.GambitApplication.class)
+public class GambitApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
